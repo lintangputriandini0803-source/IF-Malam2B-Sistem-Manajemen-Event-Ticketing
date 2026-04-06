656619028a4f72069cc6b24b87cfc3504a720169
MuhammadRafli-arch <albatamyrafli@gmail.com>
2026-04-06 20:26:59 +0700

    menambahkan login controller dan view

--- eventticketing/routes/web.php
+++ eventticketing/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+Route::get('/login', [LoginController::class,'index']);
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::get('/dashboard', function () {
+    return "Berhasil Login, ini Dashboard";
 });
