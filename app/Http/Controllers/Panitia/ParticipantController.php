62efa85066903bb41c1fb38b4525ed2cba1ac775
LINTANG PUTRI_ANDINI_NAME <lintangputriandini0803@gmail.com>
2026-04-28 16:51:49 +0700

    report peserta

--- app/Http/Controllers/Panitia/ParticipantController.php
+++ app/Http/Controllers/Panitia/ParticipantController.php
@@ -7,5 +7,15 @@ use Illuminate\Http\Request;
 
 class ParticipantController extends Controller
 {
-    //
+    public function report()
+{
+    // Contoh data dummy untuk belajar (nanti bisa diganti query database)
+    $peserta = [
+        ['nama' => 'Lintang Putri', 'email' => 'lintang83@simetix.com', 'status' => 'Lunas'],
+        ['nama' => 'Muhammad Rafli', 'email' => 'rafliiu@simetix.com', 'status' => 'Pending'],
+        ['nama' => 'Dinda Amalia', 'email' => 'dindanugroho@simetix.com', 'status' => 'Lunas'],
+    ];
+
+    return view('panitia.report_peserta', compact('peserta'));
+}
 }
